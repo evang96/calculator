bfb6d604f24e39a0712f7848d9fd32e1f66c0205
Evan Garvey <g00322695@gmit.ie>
2020-03-06 16:16:50 +0000

    first commit in my Calculator repository

---
+++ src/main/java/ie/gmit/calculator.java
@@ -0,0 +1,24 @@
+//Evan Garvey
+package ie.gmit;
+
+public class calculator {
+    private int firstNum;
+    private int secondNum;
+
+    public calculator() {
+        this.firstNum = 0;
+        this.secondNum = 0;
+    }
+
+    public calculator(int firstNum, int secondNum) { //Constructor
+        this.firstNum = firstNum;
+        this.secondNum = secondNum;
+    }
+
+    public int add(int num1, int num2)
+    {
+      int result = num1 + num2;
+      return result;
+    }
+
+}
